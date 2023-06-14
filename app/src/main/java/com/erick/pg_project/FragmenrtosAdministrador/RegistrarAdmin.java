78c9cc151e230093189c2c9cee66e008ee05a4d8
LANDEROWSKI <110938474+LANDEROWSKI@users.noreply.github.com>
2023-06-14 13:04:11 -0600

    Login para Administradores ya creados

--- app/src/main/java/com/erick/pg_project/FragmenrtosAdministrador/RegistrarAdmin.java
+++ app/src/main/java/com/erick/pg_project/FragmenrtosAdministrador/RegistrarAdmin.java
@@ -1,66 +1,155 @@
 package com.erick.pg_project.FragmenrtosAdministrador;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.erick.pg_project.MainActivityAdministrador;
 import com.erick.pg_project.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RegistrarAdmin#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+
+
+
 public class RegistrarAdmin extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    TextView FechaRegistro;
+    EditText Correo, Password, Nombres, Apellidos, Edad;
+    Button Registrar;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RegistrarAdmin() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegistroAdmin.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegistrarAdmin newInstance(String param1, String param2) {
-        RegistrarAdmin fragment = new RegistrarAdmin();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FirebaseAuth auth;
+    ProgressDialog progressDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro_admin, container, false);
+        View view = inflater.inflate(R.layout.fragment_registrar_admin, container, false);
+        FechaRegistro = view.findViewById(R.id.FechaRegistro);
+        Correo = view.findViewById(R.id.Correo);
+        Password = view.findViewById(R.id.Password);
+        Nombres = view.findViewById(R.id.Nombres);
+        Apellidos = view.findViewById(R.id.Apellidos);
+        Edad = view.findViewById(R.id.Edad);
+        Registrar = view.findViewById(R.id.Registrar);
+        //Inicializamos Firebase Autenticacion
+        auth = FirebaseAuth.getInstance();
+        Date date = new Date();
+        SimpleDateFormat fecha = new SimpleDateFormat("d 'de' MMMM 'del' yyyy");
+        String SFecha = fecha.format(date);
+        FechaRegistro.setText(SFecha);
+
+        //Al hacer Click en Registrar
+        Registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Convertimos a string correo y contrasenia
+                String correo = Correo.getText().toString();
+                String pass = Password.getText().toString();
+                String nombre = Nombres.getText().toString();
+                String apellidos = Apellidos.getText().toString();
+                String edad = Edad.getText().toString();
+
+                if(correo.equals("") || pass.equals("") || nombre.equals("") || apellidos.equals("")
+                || edad.equals(""))
+                {
+                    Toast.makeText(getActivity(), "POR FAVOR LLENE TODOS LOS CAMPOS", Toast.LENGTH_SHORT).show();
+                }else {
+                    //Validacion de Correo
+                    if(!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+                        Correo.setError("Correo Inválido Tilín");
+                        Correo.setFocusable(true);
+                    }
+                    else if(pass.length()<6){
+                        Password.setError("Contraseña debe ser mayor o igual que 6");
+                        Correo.setFocusable(true);
+                    }else{
+                        RegistroAdministradores(correo,pass);
+                    }
+                }
+
+
+            }
+        });
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Registrando, espere por favor");
+        progressDialog.setCancelable(false);
+
+
+        return view;
+    }
+    //Metodo para Registrar Administradores
+    private void RegistroAdministradores(String correo, String pass) {
+
+        progressDialog.show();
+        auth.createUserWithEmailAndPassword(correo,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                //If por si el Administreador fue creado correctamente
+                if(task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    FirebaseUser user = auth.getCurrentUser();
+                    assert user != null;//Afirmamos que el admion no es nulo
+
+                    //Convertir a Stirng los datos de los administradores
+                    String UID = user.getUid();
+                    String correo = Correo.getText().toString();
+                    String pass = Password.getText().toString();
+                    String nombre = Nombres.getText().toString();
+                    String apellidos = Apellidos.getText().toString();
+                    String edad = Edad.getText().toString();
+                    int EdadInt = Integer.parseInt(edad);
+
+                    HashMap<Object, Object> Administradores = new HashMap<>();
+                    Administradores.put("UID", UID);
+                    Administradores.put("CORREO", correo);
+                    Administradores.put("PASSWORD", pass);
+                    Administradores.put("NOMBRES", nombre);
+                    Administradores.put("APELLIDOS", apellidos);
+                    Administradores.put("EDAD", EdadInt);
+                    Administradores.put("IMAGEN", "");
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference reference = database.getReference("BASE DE DATOS ADMINISTRADORES");
+                    reference.child(UID).setValue(Administradores);
+                    startActivity(new Intent(getActivity(), MainActivityAdministrador.class));
+                    Toast.makeText(getActivity(), "REGISTRO EXITOSO", Toast.LENGTH_SHORT).show();
+                    getActivity().finish();
+                }else {
+                    progressDialog.dismiss();
+                    Toast.makeText(getActivity(), "HA OCURRIDO UN ERROR", Toast.LENGTH_SHORT).show();
+                }
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getActivity(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/erick/pg_project/MainActivityAdministrador.java
+++ app/src/main/java/com/erick/pg_project/MainActivityAdministrador.java
@@ -8,6 +8,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -17,9 +18,14 @@ import com.erick.pg_project.FragmenrtosAdministrador.ListaAdmin;
 import com.erick.pg_project.FragmenrtosAdministrador.PerfilAdmin;
 import com.erick.pg_project.FragmenrtosAdministrador.RegistrarAdmin;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivityAdministrador extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,9 @@ public class MainActivityAdministrador extends AppCompatActivity implements Navi
 
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        firebaseAuth = firebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
 
         //Fragmento por defecto
         if(savedInstanceState == null)
@@ -68,9 +77,35 @@ public class MainActivityAdministrador extends AppCompatActivity implements Navi
                         new ListaAdmin()).commit();
                 break;
             case R.id.Salir:
-                Toast.makeText(this, "Nos vemos luego Tilín", Toast.LENGTH_SHORT).show();;
+                CerrarSesion();
+                break;
         }
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void ComprobandoInicioSesion(){
+        if(user!=null)
+        {
+            //Si el administrador ha iniciado sesion
+            Toast.makeText(this, "SE HA INICIADO SESIÓN", Toast.LENGTH_SHORT).show();
+        }else{
+            //Si no ha iniciado sesion es porque el usuario es un cliente
+            startActivity(new Intent(MainActivityAdministrador.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    private void CerrarSesion()
+    {
+        firebaseAuth.signOut();
+        startActivity(new Intent(MainActivityAdministrador.this,MainActivity.class));
+        Toast.makeText(this, "CERRASTE SESIÓN CORRECTAMENTE", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        ComprobandoInicioSesion();
+        super.onStart();
     }
 }
