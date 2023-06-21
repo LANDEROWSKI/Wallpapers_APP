e37c22a7ee3bcfac571d6489045a418dc3e40284
LANDEROWSKI <110938474+LANDEROWSKI@users.noreply.github.com>
2023-06-20 21:55:39 -0600

    Creamos los menus para Agregar y Listar las Imagenes

--- app/src/main/java/com/erick/pg_project/FragmenrtosAdministrador/InicioAdmin.java
+++ app/src/main/java/com/erick/pg_project/FragmenrtosAdministrador/InicioAdmin.java
@@ -1,5 +1,6 @@
 package com.erick.pg_project.FragmenrtosAdministrador;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.erick.pg_project.CategoriasAdmin.AnimesA;
+import com.erick.pg_project.CategoriasAdmin.PeliculasA;
+import com.erick.pg_project.CategoriasAdmin.ProgramacionA;
+import com.erick.pg_project.CategoriasAdmin.VideosA;
 import com.erick.pg_project.R;
 
 
 public class InicioAdmin extends Fragment {
+
+    Button Animes, Peliculas, Programacion, Videos;
 
 
     @Override
@@ -19,6 +27,48 @@ public class InicioAdmin extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_inicio_admin, container, false);
+
+        Animes = view.findViewById(R.id.Animes);
+        Peliculas = view.findViewById(R.id.Pelicuas);
+        Programacion = view.findViewById(R.id.Programacion);
+        Videos = view.findViewById(R.id.Videojuegos);
+
+        Animes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), AnimesA.class));
+
+            }
+        });
+
+        Peliculas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), PeliculasA.class));
+
+
+            }
+        });
+
+        Programacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), ProgramacionA.class));
+
+
+            }
+        });
+
+        Videos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), VideosA.class));
+
+
+            }
+        });
+
+
         return view;
     }
 }
